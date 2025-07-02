7310fa6a0b70bf8f1f37e2ddabcd1d36899afc20
Samchan Lee <samchan.lee@gmail.com>
2025-07-02 16:57:06 +0545

    Update

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,23 @@ const https = require('https');
 const axios = require('axios');
 import express, {Request, Response} from 'express';
 
+
 const app = express()
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('*', (_: Request, res: Response) => {
   res.send('Mediator online')
 });
 
+
 var certFilePath = '/home/samchanthunder/certs/crCerts/openmrs_cert.pem';
 var keyFilePath = '/home/samchanthunder/certs/crCerts/openmrs_key.pem';
 var caCertFilePath = '/home/samchanthunder/certs/crCerts/server_cert.pem';
+
 
 const httpsAgent = new https.Agent({
   cert: fs.readFileSync(certFilePath),
@@ -28,18 +33,30 @@ const httpsAgent = new https.Agent({
   rejectUnauthorized: false,
 })
 
+
 // When a post request is made to /fhir/Patient in OpenHIM
 app.post('/fhir/Patient', async (req: Request, res: Response) => {
   const requestBody = req.body;
   if (!requestBody) {
     console.log("Invalid Request Body");
     return res.status(400).send("Invalid Request Body");
+  }else{
+    if(requestBody.identifier){
+      requestBody.identifier[0].system = "http://clientregistry.org/" + requestBody.identifier[0].system;
+      console.log("Payload sent to CR:", JSON.stringify(requestBody, null, 2));
+    }else{
+      console.error("The request body has no identifier array.")
+    }
   }
+
 
   try{
     const axiosResponse = await axios.post(CR.url, requestBody, { httpsAgent, headers: { 'Content-Type': 'application/json' } });
     res.status(axiosResponse.status).json(axiosResponse.data);
-    console.log("Success");
+    console.log("OpenCR Response Data:", axiosResponse.data);
+
+
+    console.log("Successs");
   }catch (error: any){
     console.error("Error in posting patient to CR");
      if (error.response) {
@@ -52,12 +69,12 @@ app.post('/fhir/Patient', async (req: Request, res: Response) => {
       console.error("Axios error config:", error.config);
     }
   }
-    
 })
+
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-  
+ 
   registerMediator(OPENHIM, mediatorConfig, (err: any) => {
   if (err) {
     throw new Error(`Failed to register mediator. Check your Config. ${err}`)
@@ -65,5 +82,6 @@ if (process.env.NODE_ENV !== 'test') {
   console.log("Successful registration")
 });
 }
+
 
 export default app;
